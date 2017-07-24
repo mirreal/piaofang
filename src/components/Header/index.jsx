99cb0e2222ee7ac50d4816a13c5622b73dc909a1
mirreal <zgkm181142521@gmail.com>
2017-07-24 16:27:29 +0800

    update: add demo

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,17 @@
+/**
+ * @Author: Patrick Ellison <mirreal>
+ * @Date:   2017-07-24T16:04:34+08:00
+ * @Email:  zgkm181142521@gmail.com
+ * @Last modified by:   mirreal
+ * @Last modified time: 2017-07-24T16:13:51+08:00
+ * @License: MIT
+ */
+
+
+import React from 'react'
+
+export default function Header(props) {
+    return (
+        <header>Header ...</header>
+    )
+}
---
+++ src/entry/index.jsx
@@ -0,0 +1,19 @@
+/**
+ * @Author: Patrick Ellison <mirreal>
+ * @Date:   2017-07-24T13:05:25+08:00
+ * @Email:  zgkm181142521@gmail.com
+ * @Last modified by:   mirreal
+ * @Last modified time: 2017-07-24T16:07:11+08:00
+ * @License: MIT
+ */
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Header from '../components/Header/'
+
+import '../common/css/base.css'
+
+ReactDOM.render(
+    <Header />,
+    document.getElementById('main-page')
+)
